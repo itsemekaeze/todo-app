b6d450c77a2c183e9681825a87b704d1644b3e49
itsemekaeze <itsemekaeze903@gmail.com>
2024-08-04 02:02:07 +0100

    Initial commit

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index, name="home"),
+    path('add_todo/', views.add_todo),
+    path('delete_todo/<int:todo_id>/', views.delete_todo)
+
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
+from .models import Todo
+from django.utils import timezone
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    todo_items = Todo.objects.all().order_by("-added_date")
+
+    return render(request, 'main/index.html', {
+        "todo_items": todo_items
+    })
+
+
+def add_todo(request):
+    current_date = timezone.now()
+    content = request.POST["content"]
+    todo = Todo.objects.create(added_date=current_date, text=content)
+    return HttpResponseRedirect("/")
+
+
+def delete_todo(request, todo_id):
+    todo = Todo.objects.get(id=todo_id)
+    todo.delete()
+    return HttpResponseRedirect("/")
+
